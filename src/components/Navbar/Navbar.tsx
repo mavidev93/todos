04f8a47ee3843ca87f195a161d8352356f0d0d47
mavi <mavidev93@gmail.com>
2024-06-14 17:47:43 +0330

    Navbar component added

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+function Navbar() {
+
+    return <section className={'bg-blue-600'}>
+        <nav className={' p-4 max-w-[1268px] mx-auto'}>
+            <span className={'text-white'}>TODOS </span>
+        </nav>
+    </section>
+}
+
+export default Navbar
